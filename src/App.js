b2fdec0411fc8d1ddbaa6719386f05c18d6ba21f
Межейников Владимир Леонидович <vmezheynikov@gomel.iba.by>
2020-10-19 11:02:03 +0300

    task N2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Card from './Card';
 
-class App extends Component {
-    state = {
-        inStyle: {},
-    };
-
-    checkBoxChangeHandler = (event) => {
-        if (event.target.checked) {
-            this.setState({ inStyle: { color: 'red' } });
-        } else {
-            this.setState({ inStyle: { color: 'green' } });
-        }
-    };
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <p>Test application.</p>
-                </header>
-                <Card
-                    title="Test card title"
-                    changed={this.checkBoxChangeHandler}
-                    inStyle={this.state.inStyle}>
-                    Inner card text.
-                </Card>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Test application.</p>
+            </header>
+            <Card title="Test card title">Inner card text.</Card>
+        </div>
+    );
 }
 
 export default App;
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 
-const Card = (props) => {
-    return (
-        <div className="Card" style={props.inStyle}>
-            <h1>
-                {props.title} <input type="checkbox" onClick={props.changed} />
-            </h1>
-            <hr />
-            <p>{props.children}</p>
-        </div>
-    );
-};
+class Card extends Component {
+    state = {
+        cardStyle: {},
+    };
+
+    checkBoxChangeHandler = (event) => {
+        if (event.target.checked) {
+            this.setState({ cardStyle: { color: 'red' } });
+        } else {
+            this.setState({ cardStyle: { color: 'green' } });
+        }
+    };
+
+    render() {
+        return (
+            <div className="Card" style={this.state.cardStyle}>
+                <h1>
+                    {this.props.title}{' '}
+                    <input
+                        type="checkbox"
+                        onClick={this.checkBoxChangeHandler}
+                    />
+                </h1>
+                <hr />
+                <p>{this.props.children}</p>
+            </div>
+        );
+    }
+}
 
 export default Card;
