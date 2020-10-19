4440c18602c763606d0dd3ea6192f1cd9d169ed0
Межейников Владимир Леонидович <vmezheynikov@gomel.iba.by>
2020-10-19 07:17:11 +0300

    task N2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Test application.
-        </p>
-      </header>
-      <Card title="Test card title">Inner card text.</Card>
-    </div>
-  );
+class App extends Component {
+    state = {
+        inStyle: {},
+    };
+
+    checkBoxChangeHandler = (event) => {
+        if (event.target.checked) {
+            this.setState({ inStyle: { color: 'red' } });
+        } else {
+            this.setState({ inStyle: { color: 'green' } });
+        }
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Test application.</p>
+                </header>
+                <Card
+                    title="Test card title"
+                    changed={this.checkBoxChangeHandler}
+                    inStyle={this.state.inStyle}>
+                    Inner card text.
+                </Card>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Card/index.js
+++ src/Card/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  return (
-    <div className="Card">
-       <h1>{props.title}</h1>
-       <hr/>
-       <p>{props.children}</p>
-    </div>
-  )
-}
+    return (
+        <div className="Card" style={props.inStyle}>
+            <h1>
+                {props.title} <input type="checkbox" onClick={props.changed} />
+            </h1>
+            <hr />
+            <p>{props.children}</p>
+        </div>
+    );
+};
 
 export default Card;
